d243d5e160cc72fdb3e583551b945cb76d8a0265
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-04-24 15:36:21 +0900

    トップページ完成

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -4,19 +4,28 @@
       各種行政機関の<br>COVID-19対策まとめ
     </h2>
     <v-row class="text-center">
-      <v-flex xs12 sm12 md4 text-center class="pa-3 mh-10">
-        <v-card min-height="400px" class="pa-3">
-          <p class="description">
-            浜松市では、
-          </p>
-          <p
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            class="mx-1"
-            target="_blank"
-          >
-            <a :href="link.href" target="_blank">{{ link.text }}</a>
-          </p>
+      <v-flex 
+        v-for="(information, index) in informations"
+        v-bind:key="index"
+        xs12 sm6 md4 text-center
+        class="pa-3 mh-10"
+      >
+        <v-card class="pa-3">
+          <v-card-title>
+            {{ information.title }}
+          </v-card-title>
+          <v-card-text>
+            <p class="scrollArea">
+              {{ information.text }}
+            </p>
+            <v-btn
+              :href="information.url"
+              target="_blank"
+              class="primary"
+            >
+              go
+            </v-btn>
+          </v-card-text>
         </v-card>
       </v-flex>
 
@@ -25,35 +34,20 @@
 </template>
 
 <script>
+  import informations from '../data/informations.json'
   export default {
     name: 'Top',
-
-    data: () => ({
-      importantLinks: [
-        {
-          text: '新型コロナウイルス感染症拡大防止サイト',
-          href: 'https://stopcovid19-hamamatsu.netlify.com/',
-        },
-        {
-          text: '防止サイトGitHub',
-          href: 'https://github.com/code-for-hamamatsu',
-        },
-        {
-          text: 'このサイトのGitHub',
-          href: 'https://github.com/matsuihidetoshi/mochikaeru20200414',
-        },
-      ],
-      map: "https://www.google.com/maps/d/embed?mid=1znYQhKGizcZab2ZTFwQ8r88Kekl1IiQn&hl=ja",
-    }),
+    data () {
+      return {
+        informations: informations,
+        offsetTop: 0,
+      }
+    }
   }
 </script>
 <style>
-.description span {
-  display: inline-block;
-}
-.map {
-    width: 90%;
-    margin: 5%;
-    height: 400px;
+.scrollArea {
+  height:200px;
+  overflow:scroll;
 }
 </style>
